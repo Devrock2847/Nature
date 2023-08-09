a60bf40ed6563d914d18cec934a19db277afde3e
Devrock2847 <93879277+Devrock2847@users.noreply.github.com>
2023-08-09 20:24:01 +0100

    The bear now dies at 0hp

--- Nature.cpp
+++ Nature.cpp
@@ -5,10 +5,11 @@
 int runGame() {
     Druid_Abilities druid_Abilities;
     Enemy enemy;
+    bool trigger_Bool = true;
     int a = 0;
     int b = 200;
     std::cout << "You are walking through the forest and a bear attacks" << std::endl;
-    while (true) {
+    while (trigger_Bool) {
         std::cout << "The bear has " << b << "hp" << std::endl;
         std::cout << "[1]Throw Spear  " << "[2]Savage Roar  " << "[3]Transform  " << "[4]Run Away" << std::endl;
         int player_Choice;
@@ -20,15 +21,19 @@ int runGame() {
             std::cout << "You attack with a spear and deal: " << a << std::endl;
             if (b <= 0) {
                 std::cout << "You have defeated the bear!" << std::endl;
+                trigger_Bool = false;
                 break;
             }
-            std::cout << "The bears health is now " << b << std::endl;
             break;
         case 2:
             a = druid_Abilities.savageRoar();
             b = enemy.enemy_Health(a, b);
             std::cout << "You attack with a terrifying roar and deal: " << a << std::endl;
-            std::cout << "The bears health is now " << b << std::endl;
+            if (b <= 0) {
+                std::cout << "You have defeated the bear!" << std::endl;
+                trigger_Bool = false;
+                break;
+            }
             break;
         case 3:
             break;
@@ -38,9 +43,11 @@ int runGame() {
             break;
         }
     }
+    return 0;
 }
 
 int main() {
     std::cout << "Welcome to Nature!" << std::endl;
     runGame();
+    return 0;
 }
