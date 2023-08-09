e59a2ec869a62e9c75d4fa7a7c69e793f5552463
Devrock2847 <93879277+Devrock2847@users.noreply.github.com>
2023-08-09 20:27:43 +0100

    Increased damage of Druid abilities

--- Druid_Abilities.cpp
+++ Druid_Abilities.cpp
@@ -11,13 +11,13 @@ int Druid_Abilities::transform() {
 int Druid_Abilities::savageRoar() {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator(seed);
-	std::uniform_int_distribution<int> roar_Damage(4, 8);
+	std::uniform_int_distribution<int> roar_Damage(10, 20);
 	return roar_Damage(generator);
 };
 
 int Druid_Abilities::spearThrow() {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator(seed);
-	std::uniform_int_distribution<int> spear_Damage(10, 15);
+	std::uniform_int_distribution<int> spear_Damage(20, 50);
 	return spear_Damage(generator);
 };
