0f128dd98ad6f801d4fe6fbbe2a3fa94bb38ec7c
Devrock2847 <93879277+Devrock2847@users.noreply.github.com>
2023-08-23 22:59:28 +0100

    Updated Emeny.cpp class to include enemey_type

--- Enemy.h
+++ Enemy.h
@@ -3,7 +3,7 @@
 
 class Enemy {
 public:
-	std::vector<std::string> enemy_Type(int a);
+	std::string enemy_Type(int a);
 	int enemy_Health(int a, int b);
 	int enemy_AttackPWR();
 	int regen_Rate();
