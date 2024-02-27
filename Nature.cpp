29847d4f2bd0368e078691be1688cdddb8833da5
devrock2847 <kilrix@hotmail.com>
2024-02-27 11:07:21 +0000

    Fixed issue with enemy_HP not loading correctly

--- Nature.cpp
+++ Nature.cpp
@@ -30,7 +30,7 @@ int runGame() {
     Druid_Abilities druid_Abilities;
     bool trigger_Bool = true;
     int attack_Var = 0;
-    //int enemy_HP = 200;
+    int enemy_HP = 200;
     bool isTrue = true;
     int headsOrTails;
     headsOrTails = rand() % 2 + 1;
@@ -40,23 +40,23 @@ int runGame() {
     else {
         wolfAttacks(); 
     }
-    
-
+    Enemy enemyBear(200, 40, 20);
+    enemyBear.enemy_HP = 200;
+    enemyBear.attack_Power = 40;
+    enemyBear.regen_R = 20;
 
     while (isTrue) {
-        Enemy enemy(200, 20, 40);
-        
         while (trigger_Bool) {
-            std::cout << "The bear has " << enemy.enemy_HP << "hp" << std::endl;
+            std::cout << "The bear has " << enemyBear.enemy_HP  << "hp" << std::endl;
             std::cout << "[1]Throw Spear  " << "[2]Savage Roar  " << "[3]Transform  " << "[4]Run Away" << std::endl;
             int player_Choice;
             std::cin >> player_Choice;
             switch (player_Choice) {
             case 1:
                 attack_Var = druid_Abilities.spearThrow();
-                enemy.enemy_HP = enemy.enemy_Health(attack_Var, enemy.enemy_HP);
+                enemyBear.enemy_HP = enemyBear.enemy_Health(attack_Var, enemyBear.enemy_HP);
                 std::cout << "You attack with a spear and deal: " << attack_Var << " damage" << std::endl;
-                if (enemy.enemy_HP <= 0) {
+                if (enemyBear.enemy_HP <= 0) {
                     std::cout << "You have defeated the bear!" << std::endl;
                     trigger_Bool = false;
                     break;
@@ -64,9 +64,9 @@ int runGame() {
                 break;
             case 2:
                 attack_Var = druid_Abilities.savageRoar();
-                enemy.enemy_HP = enemy.enemy_Health(attack_Var, enemy.enemy_HP);
+                enemyBear.enemy_HP = enemyBear.enemy_Health(attack_Var, enemyBear.enemy_HP);
                 std::cout << "You attack with a terrifying roar and deal: " << attack_Var << std::endl;
-                if (enemy.enemy_HP <= 0) {
+                if (enemyBear.enemy_HP <= 0) {
                     std::cout << "You have defeated the bear!" << std::endl;
                     trigger_Bool = false;
                     break;
