4cba93f628fea3cfe44c74907e68f0deeb471098
devrock2847 <kilrix@hotmail.com>
2024-02-27 12:21:53 +0000

    Added random numbers to Druid_Abilities_Transform abilites

--- Druid_Abilities_Transform.cpp
+++ Druid_Abilities_Transform.cpp
@@ -10,12 +10,12 @@ int Druid_Abilities_Transform::transform() {
 int Druid_Abilities_Transform::swipe() {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator(seed);
-	std::uniform_int_distribution<int> roar_Damage(10, 20);
-	return roar_Damage(generator);
-	int swipe_Damage = 10;
-	return swipe_Damage;
+	std::uniform_int_distribution<int> swipe_Damage(10, 20);
+	return swipe_Damage(generator);
 }
 int Druid_Abilities_Trasform::bite() {
-	int bite_Damage = 40;
-	return
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine generator(seed);
+	std::uniform_int_distribution<int> bite_Damage(30, 40);
+	return bite_Damage(generator);
 }
