c1d0fb07a3f8157c22ed0d526bb3ebe01aea211f
devrock2847 <kilrix@hotmail.com>
2024-02-27 12:33:47 +0000

    Added Druid_Abilites_Transform.h

--- Druid_Abilities_Transform.cpp
+++ Druid_Abilities_Transform.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <chrono>
 #include <random>
+#include "Druid_Abilities_Transform.h"
 
 int Druid_Abilities_Transform::transform() {
 	int transform_Damage = 0;
@@ -13,7 +14,7 @@ int Druid_Abilities_Transform::swipe() {
 	std::uniform_int_distribution<int> swipe_Damage(10, 20);
 	return swipe_Damage(generator);
 }
-int Druid_Abilities_Trasform::bite() {
+int Druid_Abilities_Transform::bite() {
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine generator(seed);
 	std::uniform_int_distribution<int> bite_Damage(30, 40);
---
+++ Druid_Abilities_Transform.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+class Druid_Abilities_Transform {
+public:
+	int transform();
+	int swipe();
+	int bite();
+};
