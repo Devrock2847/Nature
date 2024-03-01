3b10a3695caced3fe90286c274e4c09e74f76253
devrock2847 <kilrix@hotmail.com>
2024-03-01 17:54:40 +0000

    Removed excess code

--- Nature.cpp
+++ Nature.cpp
@@ -3,30 +3,6 @@
 #include "Enemy.h"
 #include <iostream>
 
-void wolfAttacks() {
-    std::cout << "You are sneaking past a cave and hear a wolf growl" << std::endl;
-    std::cout << "Suddenly 3 wolves appear and have you surrounded" << std::endl;
-    std::cout << "    +   +    +   +    +   +" << "\n";
-    std::cout << "   + + + +  + + + +  + + + +" << "\n";
-    std::cout << "  {  @ @___{  @ @___{  @ @___" << "\n";
-    std::cout << "  {    vvvv{    vvvv{    vvvv" << "\n";
-    std::cout << "   {   ^^^^ {   ^^^^ {   ^^^^" << "\n";
-    std::cout << "     ¬¬¬¬¬¬    ¬¬¬¬¬    ¬¬¬¬¬" << std::endl;
-}
-void bearAttacks() {
-    std::cout << "You are walking through the forest and a bear attacks" << std::endl;
-    std::cout << "    +     + " << "\n";
-    std::cout << "   + +   + +" << "\n";
-    std::cout << "  + ^ + + ^ +" << "\n";
-    std::cout << " {           }" << "\n";
-    std::cout << "{   @     @   }" << "\n";
-    std::cout << "{    |   |    }   " << "\n";
-    std::cout << "{    (oYo)    }" << "\n";
-    std::cout << " {    vvv    } " << "\n";
-    std::cout << "  ~~~ ^^^ ~~~" << "\n";
-    std::cout << "      ~~~" << std::endl;
-}
-
 int runGame() {
     Druid_Abilities_Human druid_Abilities_Human;
     Druid_Abilities_Transform druid_Abilities_Transform;
@@ -38,12 +14,7 @@ int runGame() {
     for (int i = 1; i <= 2; i++) {
         trigger_Bool = true;
         Enemy enemy(i);
-        if (i == 1) {
-        //    bearAttacks();
-        }
-        else {
-        //    wolfAttacks();
-        }
+
         while (trigger_Bool) {
             std::cout << "The " << enemy.get_Enemy_Type() << " has " << enemy.enemy_HP << "hp" << std::endl;
             if (transform_Bool == false) {
