718a2fff1d7bea2077dd242408990efe5ca33281
devrock2847 <kilrix@hotmail.com>
2024-03-03 02:27:21 +0000

    Added functionality to the barbarian abilites class

--- Barbarian_Abilities.cpp
+++ Barbarian_Abilities.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
 #include "Barbarian_Abilities.h"
 
 int Barbarian_Abilities::heroic_Smash() {
-	return 0;
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine generator(seed);
+	std::uniform_int_distribution<int> smash_Damage(25, 40);
+	return smash_Damage(generator);
 }
 
 int Barbarian_Abilities::thunder_Cleave() {
-	return 0;
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine generator(seed);
+	std::uniform_int_distribution<int> cleave_Damage(10, 20);
+	return cleave_Damage(generator);
 }
 
 int Barbarian_Abilities::enrage() {
+	//attack power increases 
+	//regen rate increases 
 	return 0;
 }
